45b248f89f97f68fb2cd337340913b68e707552a
DuarteRodrigues <duarte.log.2001@gmail.com>
2024-09-19 18:49:26 +0100

    Programação Concorrente e Distribuída - Programa Sequencial

---
+++ Programação Concorrente e Distribuida/Primeira Aula/Sequencial.java
@@ -0,0 +1,30 @@
+public class Sequencial {
+    
+    public static void main(String[] args) throws InterruptedException{
+        //Delay times
+        final int DELAY1 = 3000;
+        final int DELAY2 = 3000;
+
+        int proximo1 = DELAY1;
+        int proximo2 = DELAY2;
+
+        // While loop
+        while(true){
+            if (proximo1 < proximo2) {
+                Thread.sleep(proximo1);
+                System.out.println("Duarte");
+                proximo2 = proximo2 - proximo1;
+                proximo1 = DELAY1;
+            }
+            else if (proximo2 < proximo1) {
+                Thread.sleep(proximo2);
+                System.out.println("Rodrigues");
+                proximo1 = proximo2 - proximo1;
+                proximo2 = DELAY2;
+            }
+            else{
+
+            }
+        }
+    }
+}
