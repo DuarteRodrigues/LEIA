67d461eb3c5e4cdfb9ef7312ce17a612fe55e645
DuarteRodrigues <duarte.log.2001@gmail.com>
2024-10-10 17:54:10 +0100

    Corrections and Additions

---
+++ Programacao-Concorrente-e-Distribuida/Aula4/DeadlockDemo.java
@@ -0,0 +1,63 @@
+// Nº Aluno: a22206488
+// Nome: Duarte Pinto Rodrigues
+// Curso: LEIA
+// UC: Programação Concorrente e Distribuída
+// Grupo de Exercícios 3 Exercício 0
+
+import java.util.Random;
+
+public class DeadlockDemo {
+    public static void main(String[] args) {
+
+        final int NUM_PHILOS = 5;
+        final int NUM_CHOPSTICKS = NUM_PHILOS;
+        Philosopher[] philosophers = new Philosopher[NUM_PHILOS];
+        Chopstick[] chopstick = new Chopstick[NUM_CHOPSTICKS];
+
+        // Create chopsticks
+        for (int i = 0; i < NUM_CHOPSTICKS; i++) {
+            chopstick[i] = new Chopstick();
+        }
+
+        // Create philosophers
+        for (int i = 0; i < NUM_PHILOS; i++){
+            philosophers[i] = new Philosopher("Philo" + (i+1), chopstick[i], chopstick[(i+1) % NUM_CHOPSTICKS]);
+            philosophers[i].start();
+        }
+    }
+}
+class Chopstick extends Object{
+
+}
+
+class Philosopher extends Thread {
+    private Chopstick left, right;
+    private Random random;
+
+    public Philosopher(String name, Chopstick left, Chopstick right){
+        super(name);
+        this.left = left;
+        this.right = right;
+        this.random = new Random();
+    }
+
+    public void run() {
+        try{
+            while(true){
+                System.out.println(this.getName() + " thinking...");
+                Thread.sleep(random.nextInt(1000)); // Think for a while
+                synchronized(left) { // Grab left chopstick
+                    synchronized(right){ // Grab right chopstick
+                        System.out.println(this.getName() + " eating...");
+                        Thread.sleep(random.nextInt(1000)); // Eat for a while
+                    }
+                }
+            }
+        } catch (InterruptedException e){
+
+        } finally {
+
+        }
+
+    }
+}
---
+++ Programacao-Concorrente-e-Distribuida/README.md
@@ -0,0 +1,5 @@
+To compile the java files in VSC:
+
+- Using 'cd' and 'ls' commands, reach the path of the file you wish to compile
+- Type 'javac filename.java' to compile the file
+- Type 'java filename' to run the file
